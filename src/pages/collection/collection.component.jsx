c2b2a46f3b2626851914c633cddbb0ff23293450
tslilpress <tslilpress@gmail.com>
2021-01-26 18:12:04 -0500

    Created collection component

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import CollectionItem from '../../components/collection-item/collection-item.component'
+
+import './collection.styles.scss'
+
+
+const CollectionPage = ({ match }) => {
+    console.log('match', match)
+    return (
+        <div className='collection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+    )
+}
+    
+
+
+
+export default CollectionPage
