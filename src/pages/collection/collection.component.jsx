12fc3d10e3f0988b96db107684e380431f8c5d28
tslilpress <tslilpress@gmail.com>
2021-01-26 18:30:15 -0500

    matched collection id with collection names

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import CollectionItem from '../../components/collection-item/collection-item.component'
+
+import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import './collection.styles.scss'
 
 
-const CollectionPage = ({ match }) => {
-    console.log('match', match)
+const CollectionPage = ({ collection }) => {
+    console.log(collection)
     return (
         <div className='collection-page'>
         <h2>COLLECTION PAGE</h2>
@@ -15,6 +18,10 @@ const CollectionPage = ({ match }) => {
 }
     
 
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
 
 
-export default CollectionPage
+
+export default connect(mapStateToProps)(CollectionPage)
