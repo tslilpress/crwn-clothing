f75bac7f95c12d95aec9786f977b3e2e749c43e5
tslilpress <tslilpress@gmail.com>
2021-01-27 15:37:42 -0500

    fixed the shop section to map through itemss

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,6 @@
 import { createSelector } from 'reselect'
 import memoize from 'lodash.memoize'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectShop = state => state.shop
 
@@ -16,11 +9,14 @@ export const selectCollections = createSelector(
     shop => shop.collections 
 )
 
+export const selectcollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
 export const selectCollection = memoize((collectionUrlParam) => 
     createSelector(
         [selectCollections],
-        collections => 
-            collections.find(
-                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-            )
-    ))
+        collections => collections[collectionUrlParam]
+        )
+    )
